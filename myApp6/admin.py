50f0a9be8f0a22d3db09c69eea643bd79c26d91c
Bharath Sindhe <117362068+bharathsindhe03@users.noreply.github.com>
2024-07-09 20:36:47 +0530

    Create admin.py

---
+++ myApp6/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from myApp6.models import Course, Student 
+
+@admin.register(Student) 
+class StudentAdmin(admin.ModelAdmin): 
+    list_display = ('student_name','student_usn','student_sem') 
+    ordering=('student_name',) 
+    search_fields = ('student_name',) 
+admin.site.register(Course) 
+
